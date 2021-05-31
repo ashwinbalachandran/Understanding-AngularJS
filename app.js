0e7b544ec0f7bbe6ca97fb4c3c2b97dc6dfb642e
Ashwin Balachandran <ash20win@gmail.com>
2021-05-30 19:54:11 -0700

    Fix setup and directives

--- app.js
+++ app.js
@@ -1,45 +1,83 @@
 var myApp = angular.module('myApp', ['ngRoute']);
 
 myApp.config(function ($routeProvider) {
-    $routeProvider
+  $routeProvider
     .when('/', {
-        templateUrl: 'pages/main.html',
-        controller: 'mainController'
+      templateUrl: 'pages/main.html',
+      controller: 'mainController'
     })
     .when('/second', {
-        templateUrl: 'pages/second.html',
-        controller: 'secondController'
-
+      templateUrl: 'pages/second.html',
+      controller: 'secondController'
     })
     .when('/second/:num', {
-        templateUrl: 'pages/second.html',
-        controller: 'secondController'
+      templateUrl: 'pages/second.html',
+      controller: 'secondController'
+    });
+});
 
-    })
-})
+myApp.service('serviceOne', function () {
+  //   let self = this;
+  //   this.name = 'Service owner';
+  //   this.nameLength = function () {
+  //     return self.name.length;
+  //   };
+});
 
-myApp.service('serviceOne', function(){
-    let self = this;
-    this.name = 'Service owner';
-    this.nameLength = function() {
-        return self.name.length;
+myApp.controller('mainController', [
+  '$scope',
+  '$log',
+  '$timeout',
+  '$filter',
+  '$location',
+  'serviceOne',
+  function ($scope, $log, $timeout, $filter, $location, serviceOne) {
+    $scope.people = [
+      {
+        name: 'Ashwin Balachandran',
+        address: '555 Main St.',
+        city: 'New York',
+        state: 'NY',
+        zip: '11111'
+      },
+      {
+        name: 'Kartik Kunder',
+        address: '666 Satan St.',
+        city: 'Houston',
+        state: 'TX',
+        zip: '11121'
+      },
+      {
+        name: 'Anish Kaul',
+        address: '666 Mine St.',
+        city: 'New York',
+        state: 'NY',
+        zip: '11141'
+      }
+    ];
+
+    $scope.formattedAddress = function (person) {
+      return `${person.address}, ${person.city}, ${person.state}, ${person.zip}`;
+    };
+  }
+]);
+
+myApp.controller('secondController', [
+  '$scope',
+  '$log',
+  '$routeParams',
+  'serviceOne',
+  function ($scope, $log, $routeParams, serviceOne) {}
+]);
+
+myApp.directive('searchResult', function () {
+  return {
+    restrict: 'AECM',
+    templateUrl: 'directives/searchresult.html',
+    replace: true,
+    scope: {
+      personObject: '=',
+      formattedAddressFunction: '&'
     }
-})
-
-myApp.controller('mainController', ['$scope', '$log', '$timeout', '$filter', '$location', 'serviceOne', function($scope, $log, $timeout, $filter, $location, serviceOne) {
-
-    
-}]);
-
-myApp.controller('secondController', ['$scope', '$log', '$routeParams', 'serviceOne', function($scope, $log, $routeParams, serviceOne){
-
-}])
-
-
-myApp.directive("searchResult", function() {
-    return {
-        restrict: 'AECM',
-        template: '<a href="#" class="list-group-item"><h4 class="list-group-item-heading">Doe, John</h4><p class="list-group-item-text">555 Main St., New York, NY 11111</p></a>',
-        replace: true
-    }
+  };
 });
